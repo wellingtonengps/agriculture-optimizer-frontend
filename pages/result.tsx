aa18a8f003e4709449ae100ccee3c0b276473891
Patrick448 <patrickcarvalho448@gmail.com>
2022-11-18 16:55:03 -0300

    Revert "refect: result page"
    This reverts commit b3da6a6596ba8b7c0ac79f1b3e05ae25f522b5df.

--- pages/result.tsx
+++ pages/result.tsx
@@ -8,7 +8,8 @@ import SolutionCropItemList from "../components/SolutionCropItemList";
 import { Button, Card, CardResult } from "../components";
 import { BsCurrencyDollar } from "react-icons/bs";
 import { useRouter } from "next/router";
-import { Container, WrapperGrid } from "../styles/resultstyles";
+import jsPDF from "jspdf";
+import { fileURLToPath } from "url";
 
 ("jspdf");
 
@@ -47,6 +48,12 @@ const Result: NextPage = () => {
     return fields.findIndex((field) => field.id === id);
   }
 
+  function handleGeneratePdf() {
+    const modelName = "Teste";
+
+    var doc = new jsPDF("p", "pt", "a4", true);
+  }
+
   useEffect(() => {
     setLoading(true);
     fetchSolution(parseInt(query.id as string)).then((fetchedData) => {
@@ -63,40 +70,64 @@ const Result: NextPage = () => {
         <title>Agriculture | Result Optimizer</title>
       </Head>
 
-      <Container>
-        <h2>Solução {solution?.id}</h2>
-
-        <h2>Culturas</h2>
-        <WrapperGrid>
-          {itemMatrix.map((line: Array<solutionCropProps>) => (
+      <main className={styles.main}>
+        <div className={styles.body}>
+          <h2>Solução {solution?.id}</h2>
+          <div className={styles.wrapperRow}>
+            <Card
+              color="#009"
+              title="budget"
+              text={solution?.inputData.budget?.toFixed(2).toString()}
+              onClick={() => {}}
+              icon={BsCurrencyDollar}
+            />
+          </div>
+          <h2>Culturas</h2>
+          <div className={styles.wrapperGrid}>
             <div
-              key={0}
               style={{
                 display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
+                flexDirection: "column",
+                marginRight: 20,
+              }}
+            ></div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
               }}
             >
-              <h3>Planejamento ótimo</h3>
-              {line.map((el: solutionCropProps) =>
-                el ? (
-                  <CardResult key={el.id} data={el} />
-                ) : (
-                  <CardResult data={el} />
-                )
-              )}
+              {itemMatrix.map((line: Array<solutionCropProps>) => (
+                <div
+                  key={0}
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }}
+                >
+                  <h3>Campo</h3>
+                  {line.map((el: solutionCropProps) =>
+                    el ? (
+                      <CardResult key={el.id} data={el} type="full" />
+                    ) : (
+                      <CardResult data={el} type="empty" />
+                    )
+                  )}
+                </div>
+              ))}
             </div>
-          ))}
-        </WrapperGrid>
+          </div>
 
-        <div style={{ marginBottom: 10 }}>
-          {solution?.solutionCrops.map((item) => {
-            return <SolutionCropItemList key={item.id} data={item} />;
-          })}
+          <div style={{ marginBottom: 10 }}>
+            {solution?.solutionCrops.map((item) => {
+              return <SolutionCropItemList key={item.id} data={item} />;
+            })}
+          </div>
+
+          <Button title="Salvar" onClick={handleGeneratePdf} />
         </div>
-
-        <Button title="Salvar" onClick={() => {}} />
-      </Container>
+      </main>
     </>
   ) : (
     <></>
@@ -104,3 +135,36 @@ const Result: NextPage = () => {
 };
 
 export default Result;
+/*
+type itemListProps={
+  data: cropProps
+}
+
+const ItemList = (props: itemListProps) => {
+  const [active, setIsActive] = useState(false);
+
+  function handleSetActive() {
+    setIsActive(!active);
+  }
+
+  return (
+    <div
+      key={props.data.id}
+      className={styles.itemList}
+      onClick={() => handleSetActive()}
+    >
+      <span>{props.data.name}</span>
+      <div className={styles.wrapperFull}>
+        <div className={styles.input}>
+          <AiOutlineArrowUp color="#2FC52C" />
+          <span>{props.data.price}</span>
+        </div>
+        <div className={styles.output}>
+          <AiOutlineArrowDown color="#EF1818" />
+          <span>{props.data.cost}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+*/
